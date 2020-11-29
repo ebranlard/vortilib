df65aa753d85a21b3e513824dfeb3e73d04008cf
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2020-11-28 18:10:48 -0700

    Adding example for inviscid vorticity patch, and tools

--- vortilib/elements/InviscidVortexPatch.py
+++ vortilib/elements/InviscidVortexPatch.py
@@ -1,20 +1,89 @@
+""" 
+2D inviscid vortex patch (ivp)
+
+omega = (1-r**2)**k
+
+See:
+ [1] Chapter 33, p.402, Branlard - Wind turbine aerodynamics and vorticity based methods, Springer 2017
+
+
+"""
+
 import numpy as np
     
-def omega(X,Y,k=2,bPolarIn=False): 
+def ivp_omega(X,Y,k=2,polarIn=False): 
+    """ 
+    Vorticity distribution for 2D inviscid vortex patch (ivp)
+    """
     X=np.asarray(X)
     Y=np.asarray(Y)
-    if bPolarIn:
+    if polarIn:
         r = X
         theta = Y
     else:
         r = np.sqrt(X ** 2 + Y ** 2)
         theta = np.arctan2(Y,X)
-    
-    omega_z = (1-r**2)**k
+    omega_z=np.zeros(r.shape)
+    # r<=1
+    bLow=r<=1
+    rl = r[bLow]
+    omega_z[bLow] = (1-rl**2)**k
     # r>1
-    I = np.abs(r) > 1
-    omega_z[I] = 0
+    omega_z[np.abs(r)>1] = 0
     # r=0
-    I = np.abs(r) < 1e-14
-    omega_z[I] = 1
+    omega_z[np.abs(r)<1e-14] = 1
     return omega_z
+
+def ivp_Gamma(r,k=2): 
+    """ 
+    Circulation distribution for 2D inviscid vortex patch (ivp)
+    """
+    r=np.asarray(r)
+    Gamma=np.zeros(r.shape)
+    # r<=1
+    bLow=r<=1
+    rl = r[bLow]
+    Gamma[bLow] = np.pi/(k+1)* (1-(1-rl**2)**(1+k))
+    # r>1
+    Gamma[np.abs(r)>1] = np.pi/(k+1)
+    return Gamma
+
+
+def ivp_u(X, Y, k=2, polarIn=False, polarOut=False): 
+    """
+    Velocity distribution for 2D inviscid vortex patch (ivp)
+
+    if polarIn is True:
+       X=r
+       Y=theta
+    """
+    if polarIn:
+        r = X
+        theta = Y
+    else:
+        r = np.sqrt(X ** 2 + Y ** 2)
+        theta = np.arctan2(Y,X)
+    U       = np.zeros(X.shape)
+    V       = np.zeros(Y.shape)
+    Ut      = np.zeros(X.shape)
+    omega_z = np.zeros(X.shape)
+    # r<=1
+    bLow= np.logical_and(r<=1, r>1e-14)
+    rl = r[bLow]
+    Ut[bLow]       = (1 - (1-rl**2)**k +  (rl**2 *(1-rl**2)**k))/(2*(1+k)*rl)
+    omega_z[bLow]  = (1 - rl**2)**k
+    # r>1
+    bHigh=r>1
+    Ut[bHigh] = 1.0/(2*(1+k)*r[bHigh])
+    omega_z[bHigh] = 0
+    # r=0
+    bZero=r<1e-14
+    Ut[bZero]      = 0
+    omega_z[bZero] = 1
+    if polarOut:
+        U = Ut * 0
+        V = Ut
+    else:
+        U = -np.sin(theta)*Ut
+        V =  np.cos(theta)*Ut
+    return U,V,omega_z
